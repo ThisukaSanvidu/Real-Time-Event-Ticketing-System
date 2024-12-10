b10c0920d4cc925c7bfb4faf3fe12c9ac7d27b45
ThisukaSanvidu <thisukasanvidu@gmail.com>
2024-12-10 21:25:14 +0530

    Added @CrossOrigin("*") for SystemController

--- Backend-Spring Boot/RealTimeEventTicketingSystem/RealTimeEventTicketingSystem/src/main/java/com/thisuka/rtets/controller/SystemController.java
+++ Backend-Spring Boot/RealTimeEventTicketingSystem/RealTimeEventTicketingSystem/src/main/java/com/thisuka/rtets/controller/SystemController.java
@@ -5,6 +5,7 @@ import com.thisuka.rtets.model.TicketingConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class SystemController{
